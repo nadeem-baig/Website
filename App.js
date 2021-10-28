188feebc35b572f3b7228dcd494a92bf196c1197
nadeem-baig <57611120+nadeem-baig@users.noreply.github.com>
2021-10-28 22:19:47 +0530

    Added navigation

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import MainScreen from './source/screens/MainScreen'
+import Account from './source/screens/Account'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <MainScreen />
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator >
+        <Drawer.Screen name="MainScreen" component={MainScreen} 
+        options={{
+          headerShown:false,
+        }}
+        />
+        <Drawer.Screen name="Account" component={Account} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
 
---
+++ source/screens/Account.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text,StyleSheet } from 'react-native'
+const Account = () => {
+    return (
+        <View style ={styles.container}>
+            <Text>this is account screen</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+    }
+})
+export default Account
