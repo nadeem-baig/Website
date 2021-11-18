6e700e2e7c651f52c16383b69620701e441f4ccc
nadeem-baig <57611120+nadeem-baig@users.noreply.github.com>
2021-11-18 21:32:36 +0530

    data changed

---
+++ source/component/findDoctorScreen/SeedetailsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text,StyleSheet } from 'react-native'
+const SeedetailsScreen = () => {
+    return (
+        <View style ={styles.container}>
+            <Text></Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+    }
+})
+export default SeedetailsScreen
--- source/component/mainscreen/autoSearch.js
+++ source/component/mainscreen/autoSearch.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import { View, TextInput ,StyleSheet,Image, FlatList, Text,TouchableOpacity } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import color from '../../assests/colour'
 import Screen from '../Screen'
 const autoSearch = () => {
@@ -14,17 +15,19 @@ const autoSearch = () => {
     }
   }, [])
 
+
+
   const FetchData = () => {
     var requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
     
-    fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
+     fetch("https://nadeem-baig.github.io/Website/source/assests/api/medicines.json", requestOptions)
       .then(response => response.json())
     .then((responseJson) => {
       setfilterData(responseJson)
-      setmasterData(responseJson)
+      setmasterData(responseJson)      
       
     }).catch((error) => {
       console.log(error)
@@ -34,10 +37,10 @@ const autoSearch = () => {
   const ItemView =({ item}) => {
     return (
       <TouchableOpacity
-      onPress={()=>{console.log(item.title);
+      onPress={()=>{console.log(item.id);
       }}>
        <Text style={styles.itemStyle}>
-        {item.id}{' .'}{item.title.toUpperCase()}
+        {item.id}{' .'}{item.name.toUpperCase()}
       </Text>
     </TouchableOpacity>
 
@@ -55,7 +58,7 @@ const autoSearch = () => {
   const searchFilter = (text) => {
     if (text){
       const newData = masterData.filter((item) => {
-        const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase()
+        const itemData = item.name ? item.name.toUpperCase() : ''.toUpperCase()
         const textData = text.toUpperCase()
         return itemData.indexOf(textData) > -1
       })
@@ -74,6 +77,7 @@ const autoSearch = () => {
         <View style ={styles.back}>
         <View style ={styles.container}>
         <Image source = {require('../../assests/images/icon/mission.png')} style ={styles.icon}/>
+        <MaterialCommunityIcons name="format-clear" size={24} color="black" style ={styles.clear} />
         <TextInput
         style={styles.input}
         value={search}           
@@ -128,6 +132,9 @@ const styles = StyleSheet.create({
       },
       itemStyle:{
         padding:10
+      },
+      clear:{
+        alignSelf:'auto'
       }
 })
 export default autoSearch
